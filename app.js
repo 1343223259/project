59c0aa0c81f0b0fca90b5024ad79250d519035f8
未来 <7814436+oldRust@user.noreply.gitee.com>
2024-04-27 08:22:13 +0800

    121321

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({
-    port: 9527,
+    port: 8088,
 });
 
 wss.on("connection", function connection(ws) {
     console.log("客户端连接成功");
+
     ws.on("message", function message(data) {
+       
         let message = JSON.parse(data.toString())
-        switch (message.action) {
-            case 'message':
-                console.log(getTime(), message.message.user_nickName + ':' + message.message.msg_content)
-                break
-            case 'join':
-                console.log(getTime(), message.message.user_nickName + ':' + message.message.msg_content)
-                break
-        }
+        console.log(message.message,"data")
+        // switch (message.action) {
+        //     case 'message':
+        //         console.log(getTime(), message.message.user_nickName + ':' + message.message.msg_content)
+        //         break
+        //     case 'join':
+        //         console.log(getTime(), message.message.user_nickName + ':' + message.message.msg_content)
+        //         break
+        // }
         wss.clients.forEach(cen => {
             cen.send(JSON.stringify(message))
         })
@@ -25,4 +28,4 @@ function getTime() {
     return `[${new Date().toLocaleTimeString()}]`
 }
 
-console.log('打开-> http://127.0.0.1:9527')
+console.log('打开-> http://127.0.0.1:8088')
